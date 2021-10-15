6a5aa3b8dd24284eb06caeb11b4dcd7398353fd4
mmednik <mmednik@gmail.com>
2021-10-15 16:40:26 -0300

    color and public html basic changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {
-  Classes,
   Alignment,
   Navbar,
   NavbarGroup,
@@ -16,21 +15,81 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <Navbar fixedToTop>
-        <NavbarGroup align={Alignment.LEFT}>
-          <NavbarHeading>gpsVegano</NavbarHeading>
-          <NavbarDivider />
-          <Button className={Classes.MINIMAL} icon="home" text="Home" />
-          <Button className={Classes.MINIMAL} icon="document" text="Files" />
-        </NavbarGroup>
-      </Navbar>
-      <Card interactive={true} elevation={Elevation.TWO}>
-        <h5>
-          <a href="#">Card heading</a>
-        </h5>
-        <p>Card content</p>
-        <Button>Submit</Button>
-      </Card>
+      <header>
+        <Navbar fixedToTop>
+          <NavbarGroup align={Alignment.LEFT}>
+            <NavbarHeading>gpsVegano</NavbarHeading>
+            <NavbarDivider />
+            <Button intent="success" minimal={true} icon="home" text="Home" />
+            <Button
+              intent="success"
+              minimal={true}
+              icon="document"
+              text="Files"
+            />
+          </NavbarGroup>
+        </Navbar>
+      </header>
+      <main>
+        <div className="stores">
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+          <Card className="store" interactive={true} elevation={Elevation.ONE}>
+            <h5>
+              <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button>Submit</Button>
+          </Card>
+        </div>
+      </main>
     </div>
   );
 }
