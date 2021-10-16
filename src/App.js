5b0ae1140c1bbc59f5cc9b49fe1632ca161ac788
mmednik <mmednik@gmail.com>
2021-10-16 14:16:23 -0300

    base header component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import {
-  Alignment,
-  Navbar,
-  NavbarGroup,
-  NavbarHeading,
-  NavbarDivider,
   Button,
   Card,
   Elevation,
 } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+import Header from "./components/Header";
+
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Navbar fixedToTop>
-          <NavbarGroup align={Alignment.LEFT}>
-            <NavbarHeading>gpsVegano</NavbarHeading>
-            <NavbarDivider />
-            <Button intent="success" minimal={true} icon="home" text="Home" />
-            <Button
-              intent="success"
-              minimal={true}
-              icon="document"
-              text="Files"
-            />
-          </NavbarGroup>
-        </Navbar>
-      </header>
+      <Header />
       <main>
         <div className="stores">
           <Card className="store" interactive={true} elevation={Elevation.ONE}>
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Alignment,
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+  NavbarDivider,
+  Button
+} from "@blueprintjs/core";
+
+function Header() {
+  return (
+    <header>
+      <Navbar fixedToTop>
+        <NavbarGroup align={Alignment.LEFT}>
+          <NavbarHeading>gpsVegano</NavbarHeading>
+          <NavbarDivider />
+          <Button intent="success" minimal={true} icon="home" text="Home" />
+          <Button
+            intent="success"
+            minimal={true}
+            icon="document"
+            text="Files"
+          />
+        </NavbarGroup>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
