02739d7f697ad79f9c3d4733ea4f8fe1b6399f71
mmednik <mmednik@gmail.com>
2021-10-18 18:04:57 -0300

    add prismic & try fetching all stores

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import {
-  Button,
-  Card,
-  Elevation,
-} from "@blueprintjs/core";
+import { useState, useEffect } from "react";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-reactjs";
 
 import Header from "./components/Header";
 import Store from "./components/Store";
@@ -12,12 +11,54 @@ import Store from "./components/Store";
 import "./App.scss";
 
 function App() {
+  const apiEndpoint = "https://gpsvegano.cdn.prismic.io/api/v2";
+  const accessToken =
+    "MC5ZVzNZTWhBQUFCOEFZU3V6.DO-_ve-_ve-_ve-_vRDvv70Jfu-_vTTvv73vv706Ju-_ve-_vWpXVe-_vQ0677-977-977-9dO-_vXxnVu-_vQ";
+  const Client = Prismic.client(apiEndpoint, { accessToken });
+  const [docs, setDocsData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await Client.query(
+        Prismic.Predicates.at("document.type", "post")
+      );
+      if (response) {
+        setDocsData(response.results);
+        console.log(response.results);
+      }
+    };
+    fetchData();
+  }, []);
+
+  function linkResolver(doc) {
+    // Define the url depending on the document type
+    if (doc.type === "post") {
+      return "/post/" + doc.uid;
+    }
+
+    // Default to homepage
+    return "/";
+  }
+
   return (
     <div className="App">
       <Header />
       <main>
         <div className="stores">
-          <Store />
+          {docs ? (
+            docs.map(doc => (
+              <div>
+                <h1>{RichText.asText(doc.data.title)}</h1>
+                <img alt="cover" src={doc.data.image.url} />
+                <RichText
+                  render={doc.data.post_body}
+                  linkResolver={linkResolver}
+                />
+              </div>
+          ))
+          ) : (
+            <div>No content</div>
+          )}
         </div>
       </main>
     </div>
