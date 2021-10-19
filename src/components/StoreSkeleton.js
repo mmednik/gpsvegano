03d6527849d55122f67dd02c6811176e304d8420
mmednik <mmednik@gmail.com>
2021-10-19 18:34:48 -0300

    store skeleton component

--- src/components/StoreSkeleton.js
+++ src/components/StoreSkeleton.js
@@ -15,7 +15,7 @@ function Store({ index, type }) {
         >
           <h5 className="bp3-skeleton">Store</h5>
           <span className="bp3-skeleton">Tag</span>
-          <img className="bp3-skeleton" />
+          <img className="bp3-skeleton" alt="Store image" />
         </Card>
       );
 
@@ -29,7 +29,7 @@ function Store({ index, type }) {
         >
           <h5 className="bp3-skeleton">Store</h5>
           <span className="bp3-skeleton">Tag</span>
-          <img className="bp3-skeleton" />
+          <img className="bp3-skeleton" alt="Store image" />
         </Card>
       );
   }
