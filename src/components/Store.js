aa4a1e31672299bf6a59ff9fee0bd0ddaa89103e
mmednik <mmednik@gmail.com>
2021-10-21 16:54:03 -0300

    chakring store component

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box, Image, Flex, Badge, Text } from "@chakra-ui/react";
 
 import "./Store.scss";
 
@@ -16,80 +17,67 @@ function Store({
   instagram,
   facebook,
   phone,
-  email
+  email,
 }) {
-
   let offlineTag;
-  if(offline) {
-    offlineTag = <span>Local</span>
+  if (offline) {
+    offlineTag = <span>Local</span>;
   }
 
   let deliveryTag;
-  if(delivery) {
-    deliveryTag = <span>Delivery</span>
+  if (delivery) {
+    deliveryTag = <span>Delivery</span>;
   }
 
   let webLink;
-  if(web) {
-    webLink = <span>{web}</span>
+  if (web) {
+    webLink = <span>{web}</span>;
   }
 
   let instagramLink;
-  if(instagram) {
-    instagramLink = <span>{instagram}</span>
+  if (instagram) {
+    instagramLink = <span>{instagram}</span>;
   }
 
   let facebookLink;
-  if(facebook) {
-    facebookLink = <span>{facebook}</span>
+  if (facebook) {
+    facebookLink = <span>{facebook}</span>;
   }
 
   let phoneLink;
-  if(phone) {
-    phoneLink = <span>{phone}</span>
+  if (phone) {
+    phoneLink = <span>{phone}</span>;
   }
 
   let emailLink;
-  if(email) {
-    emailLink = <span>{email}</span>
+  if (email) {
+    emailLink = <span>{email}</span>;
   }
 
   switch (type) {
     case "list":
       return (
-        <div
-          className="store"
-          key={index}
-        >
-          <div className="content">
-            <a href={link}>
-              <h5 className="title">{title}</h5>
-              <span>{tag}</span>
-              {offlineTag}
-              {deliveryTag}
-              {webLink}
-              {instagramLink}
-              {facebookLink}
-              {phoneLink}
-              {emailLink}
-              <img
-                className="image"
-                src={img}
-                width="150"
-                height="150"
-                alt={`Logo de ${title}`}
-              />
-            </a>
-          </div>
-        </div>
+        <Box p="5" maxW="320px" borderWidth="1px" data-link={link}>
+          <Image borderRadius="md" src={img} alt={`Logo de ${title}`} />
+          <Flex align="baseline" mt={2}>
+            <Badge colorScheme="pink">{tag}</Badge>
+          </Flex>
+          <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+            {title}
+          </Text>
+          {offlineTag}
+          {deliveryTag}
+          {webLink}
+          {instagramLink}
+          {facebookLink}
+          {phoneLink}
+          {emailLink}
+        </Box>
       );
 
     default:
       return (
-        <div
-          className="store"
-          key={index}
-        >
+        <div className="store" key={index}>
           <h5>
             <a href="link-to-store">{title}</a>
           </h5>
