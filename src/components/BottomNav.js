fe43697bd4431e5758886daadd57d187416029bf
mmednik <mmednik@gmail.com>
2021-10-30 11:31:56 -0300

    botton nav base component

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Link } from "@chakra-ui/react";
+import { AiOutlineBarcode, AiFillShop, AiOutlineBars } from "react-icons/ai";
 
 import "./BottomNav.scss";
 
 function BottomNav() {
   return (
-    <Box bgColor="white" pos="fixed" bottom="0" width="100%" height="50px" boxShadow="lg">
-      Bottom Nav
+    <Box
+      className="bottom-nav"
+      bgColor="white"
+      pos="fixed"
+      bottom="0"
+      width="100%"
+      height="50px"
+      p={2}
+    >
+      <Flex mx={8}>
+        <Link>
+          <AiOutlineBarcode className="bottom-icon" />
+        </Link>
+        <Spacer />
+        <Link>
+          <AiFillShop className="bottom-icon" color="green" />
+        </Link>
+        <Spacer />
+        <Link>
+          <AiOutlineBars className="bottom-icon" />
+        </Link>
+      </Flex>
     </Box>
   );
 }
